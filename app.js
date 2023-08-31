d25fcfc62f81ffaacbbcee2342d4511ce94410f4
Teorema <moralesmateo546@gmail.com>
2023-08-31 01:48:18 -0300

    mi_primer_commit

--- app.js
+++ app.js
@@ -6,18 +6,3 @@ app.listen(3030, ()=> console.log('servidor corriendo en el puerto 3030'));
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'src/views/index.html'));
 });
-app.get('/babbage',(req,res)=>{
-    res.sendFile(path.join(__dirname,'src/views/babbage.html'));
-});app.get('/berners-lee',(req,res)=>{
-    res.sendFile(path.join(__dirname,'src/views/berners.html'));
-});app.get('/clarke',(req,res)=>{
-    res.sendFile(path.join(__dirname,'src/views/clarke.html'));
-});app.get('/hamilton',(req,res)=>{
-    res.sendFile(path.join(__dirname,'src/views/hamilton.html'));
-});app.get('/hopper',(req,res)=>{
-    res.sendFile(path.join(__dirname,'src/views/hopper.html'));
-});app.get('/lovelace',(req,res)=>{
-    res.sendFile(path.join(__dirname,'src/views/lovelace.html'));
-});app.get('/turing',(req,res)=>{
-    res.sendFile(path.join(__dirname,'src/views/turing.html'));
-});
